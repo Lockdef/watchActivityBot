9b966ac31bb486dd1f384346f13f33f40087e668
lockdef <locktki@icloud.com>
2020-12-06 21:05:11 +0900

    twitterのAPIKEYを追加

--- setting.py
+++ setting.py
@@ -6,3 +6,5 @@ DOTENV_PATH = os.path.join(os.path.dirname(__file__), '.env')
 dotenv.load_dotenv(DOTENV_PATH)
 
 TOKEN = os.environ.get("TOKEN")
+API_KEY = os.environ.get("API_KEY")
+API_KEY_SECRET = os.environ.get("API_KEY_SECRET")
