80ec5cc1f4c65711b6d9b9ebc1f72b3d95a95eb2
lockdef <locktki@icloud.com>
2020-12-07 15:06:05 +0900

    Finalで定数化

--- setting.py
+++ setting.py
@@ -1,10 +1,11 @@
 import os
 import dotenv
+from typing import Final
 
 dotenv.load_dotenv(verbose=True)
 DOTENV_PATH = os.path.join(os.path.dirname(__file__), '.env')
 dotenv.load_dotenv(DOTENV_PATH)
 
-TOKEN = os.environ.get("TOKEN")
-API_KEY = os.environ.get("API_KEY")
-API_KEY_SECRET = os.environ.get("API_KEY_SECRET")
+TOKEN: Final[str] = os.environ.get("TOKEN")
+API_KEY: Final[str] = os.environ.get("API_KEY")
+API_KEY_SECRET: Final[str] = os.environ.get("API_KEY_SECRET")
