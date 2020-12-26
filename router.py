57c716c22a06cd4e5c06d2a441878b8146b7294d
lockdef <locktki@icloud.com>
2020-12-26 23:17:07 +0900

    ルーティングを変更

--- router.py
+++ router.py
@@ -2,21 +2,25 @@ import os
 from threading import Thread
 import asyncio
 from flask import Flask
+from setting import DISCORD_TOKEN
 from twitterApp import TwitterApp
+from discordBot import DiscordBot
 
 app = Flask(__name__)
 app.secret_key = "key"
 twitterApp = TwitterApp()
+discordBot = DiscordBot()
+discordBot.add_username('Lock')
 
 
-@app.route('/twitter/request_token', methods=['GET'])
+@app.route('/login', methods=['GET'])
 def get_request_token():
     return twitterApp.get_request_token()
 
 
-@app.route('/twitter/callback')
+@app.route('/callback', methods=['GET'])
 def callback():
-    return twitterApp.callback()
+    return twitterApp.update_profile(discordBot.get_activity())
 
 
 if __name__ == "__main__":
@@ -25,5 +29,5 @@ if __name__ == "__main__":
     app_thread = Thread(target=app.run, args=(host, port))
     app_thread.start()
     loop = asyncio.get_event_loop()
-    loop.create_task(twitterApp.run_discord_bot())
+    loop.create_task(discordBot.start(DISCORD_TOKEN))
     Thread(target=loop.run_forever())
