d5fe0a8a7fbd10934462a60f694b616ff3ad2638
lockdef <locktki@icloud.com>
2020-12-29 22:43:58 +0900

    uidやtokenの保存機能を実装

--- router.py
+++ router.py
@@ -1,27 +1,40 @@
 import os
 from threading import Thread
 import asyncio
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 from setting import DISCORD_TOKEN
 from twitterApp import TwitterApp
 from discordBot import DiscordBot
+from db import User
 
 app = Flask(__name__)
 app.secret_key = "key"
-twitterApp = TwitterApp()
-discordBot = DiscordBot()
-discordBot.add_username('Lock', "3426")
+user = User(app)
+twitterApp = TwitterApp(user)
+discordBot = DiscordBot(user, twitterApp)
 
 
 @app.route('/', methods=['GET'])
 def index():
+    if session.keys() >= {'access_token', 'access_token_secret'}:
+        return render_template("set_uid.html")
     redirect_url = twitterApp.get_request_token()
     return render_template("index.html", redirect_url=redirect_url)
 
 
+@app.route('/', methods=['POST'])
+def add_user():
+    uid = request.form.get('uid')
+    access_token = session['access_token']
+    access_token_secret = session['access_token_secret']
+    user.add(uid, access_token, access_token_secret)
+    return "good"
+
+
 @ app.route('/callback', methods=['GET'])
 def callback():
-    return "Succeful." if twitterApp.update_profile(discordBot.get_activity("Lock", "3426")) else "Failed."
+    twitterApp.callback()
+    return redirect('/')
 
 
 if __name__ == "__main__":
