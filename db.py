2de634d7f2c08e555855daad4eec714051fba2f8
lockdef <locktki@icloud.com>
2020-12-29 16:09:45 +0900

    DB周りの操作の実装

--- db.py
+++ db.py
@@ -1,34 +1,80 @@
 import os
-import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-DB_FILENAME = "user.db"
 
-
-class UserDB():
+class User():
     """
-    ユーザーに関するデータベース
-    twitterのtokenとdiscordのusername、activity_nameを結びつける
+    ユーザーデータベースへの操作全般を扱うクラス
+
+    Parameters
+    ----------
+    app : Flask
+        Flaskのインスタンス
 
     Attributes
     ----------
-    connection : sqlite3.Connection
-        データベースを表す
-    cursor : sqlite3.Cursor
-        データベースを操作するカーソル
+    db : flask_sqlalchemy.SQLAlchemy
+        SQLAlchemyのインスタンス
+    UserModel : UserModel
+        UserModelのクラス
     """
 
-    def __init__(self):
+    def __init__(self, app: Flask):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///"user".db'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+        self.db = SQLAlchemy(app)
 
-        isExistedDB = os.path.exists(f"./{DB_FILENAME}")
+        if not os.exists("./user.db"):
+            self.db.create_all()
 
-        self.connction: sqlite3.Connection = sqlite3.connect(DB_FILENAME)
-        self.cursor: sqlite3.Cursor = self.connction.cursor()
+        class UserModel(self.db.Model):
+            """
+            ユーザーのモデルクラス
 
-        if not isExistedDB:
-            self.cursor.execute(
-                """
-                CREATE TABLE user (
-                    
-                )
-                """
-            )
+            Parameters
+            ----------
+            uid : int
+                discordユーザーのid
+            api_key : str
+                twitterのoauthのapikey
+            api_key_secret : str
+                twitterのoauthのapikeysecret
+
+            Attributes
+            ----------
+            uid : int
+                discordユーザーのid
+            api_key : str
+                twitterのoauthのapikey
+            api_key_secret : str
+                twitterのoauthのapikeysecret
+            """
+            uid = self.db.Column(self.db.Integer(17), unique=True)
+            api_key = self.db.Column(self.db.String(27), unique=True)
+            api_key_secret = self.db.Column(self.db.String(32), unique=True)
+
+            def __init__(self, uid: int, api_key: str, api_key_secret: str):
+                self.uid = uid
+                self.api_key = api_key
+                self.api_key_secret = api_key_secret
+
+        self.UserModel = UserModel
+
+    def add(self, uid: int, api_key: str, api_key_secret: str):
+        user = self.UserModel(uid, api_key, api_key_secret)
+        self.db.add(user)
+        self.db.session.commit()
+
+    def read_all(self):
+        users = self.UserModel.all()
+        return users
+
+    def read_by_uid(self, uid: str):
+        user = self.db.session.query(self.UserModel).filter_by(uid=uid).first()
+        return user
+
+    def delete(self, uid: int):
+        user = self.db.session.query(self.UserModel).filter_by(uid=uid).first()
+        self.db.session.delete(user)
+        self.db.session.commit()
