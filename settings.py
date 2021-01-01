08dc7bf310a342b5e8872fed6eb6bef594619d01
lockdef <locktki@icloud.com>
2021-01-01 19:07:37 +0900

    Finalを削除

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 import os
 import dotenv
-from typing import Final
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
@@ -8,9 +7,9 @@ dotenv.load_dotenv(verbose=True)
 DOTENV_PATH = os.path.join(os.path.dirname(__file__), '.env')
 dotenv.load_dotenv(DOTENV_PATH)
 
-DISCORD_TOKEN: Final[str] = os.environ.get("DISCORD_TOKEN")
-API_KEY: Final[str] = os.environ.get("API_KEY")
-API_KEY_SECRET: Final[str] = os.environ.get("API_KEY_SECRET")
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+API_KEY = os.environ.get("API_KEY")
+API_KEY_SECRET = os.environ.get("API_KEY_SECRET")
 
 app = Flask(__name__)
 app.secret_key = "key"
